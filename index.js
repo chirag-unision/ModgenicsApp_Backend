cdf42e5e5b9c9b5525a088e37977a2695d9dceeb
chirag-unision <sharmacj123@gmail.com>
2024-11-06 16:33:08 +0530

    test route

--- index.js
+++ index.js
@@ -33,6 +33,17 @@ app.get('/', (req, res)=>{
     res.send('Hey there! This is Modgenics App backend.'+process.env.DB_HOST);
 });
 
+app.get('/test/', (req, res)=>{
+    const randomText = () =>
+      alternateCapitals(
+          Math.random()
+              .toString(36)
+              .replace(/[^a-z0-9]+/g, '').substring(2, 8)
+    );
+    let a= randomText();
+    res.send(a);
+});
+
 //Middleware
 function middleDB(req, res, next) {
   req.db = db;
